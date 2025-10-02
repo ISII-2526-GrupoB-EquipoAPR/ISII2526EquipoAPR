f3c4acc50659f803cedae10680be3684cb293120
Adela12345678 <166329356+Adela12345678@users.noreply.github.com>
2025-10-02 21:00:17 +0200

    He creado los atributos de la clase Car
    Co-authored-by: ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>

---
+++ src/AppForSEII2526.API/Models/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Car
+{
+	public Car()
+	{
+	}
+	public Car(string carClass,string color,string description,string id,string manufacturer,decimal purchasingPrice,int quantityForPurchasing,int quantityForRenting,int rentalItems,double rentingPrice,string reviewItems)
+	{
+		CarClass = carClass;
+		Color = color;
+		Description = description;
+		Id = id;
+		Manufacturer = manufacturer;
+		PurchasingPrice = purchasingPrice;
+		QuantityForPurchasing = quantityForPurchasing;
+		QuantityForRenting = quantityForRenting;
+		RentalItems = rentalItems;
+		RentingPrice = rentingPrice;
+		ReviewItems = reviewItems;
+
+
+	}
+	
+}
