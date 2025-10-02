a4d7c899655cc0708a9404fac8ceb8067eadf357
ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>
2025-10-02 20:15:38 +0200

    Primera modificación de la clase ApplicationUser

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,19 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+    public ApplicationUser() { }
+    
+    public ApplicationUser(string id,string name,string surname,string userName)
+    {
+        Id = id;
+        Name = name;
+        Surname = surname;
+        UserName = userName;
+        Email = userName;
+        
+    }
+
+        
+    
+  
 }
