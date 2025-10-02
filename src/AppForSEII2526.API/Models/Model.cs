6a079c3730b3f1a71400d41b7993172c24ad3793
ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>
2025-10-02 20:20:54 +0200

    He creado los atributos de la clase Model

---
+++ src/AppForSEII2526.API/Models/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Model
+{
+	public Model()
+	{
+	}
+	public Model(string id, string name)
+	{
+		Id = id;
+		Name = name;
+
+	}
+	public int Id { get; set; }
+	public string Name { get; set; }
+	
+
+}
