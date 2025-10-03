03fa4fb8af0fc257528eed2f632153d3ce67a555
PATRICIA QUINTANAR MARTÍNEZ <Patricia.Quintanar@alu.uclm.es>
2025-10-03 13:40:25 +0200

    He modificado la clase Purchase

--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -1,18 +1,35 @@
-﻿using System;
-
-public class Purchase
+﻿namespace AppForSEII2526.API.Models
 {
-	public Purchase(){}
-
-	public Purchase (string deliveryCarDealer, int id, string name, string paymentMethod, DateTime purchasingDate, decimal purchasingPrice, string surname)
+	public enum PaymentMethodTypes
 	{
-		DeliveryCarDealer = deliveryCarDealer;
-		Id = id;
-		Name = name;
-		PaymentMethod = paymentMethod;
-		PurchasingDate = purchasingDate;
-		PurchasingPrice = purchasingPrice;
-		Surname = surname;
-    }
+		Visa,
+		GooglePay
+	}
+    public class Purchase
+	{
+		public string deliveryCarDealer { get; set; }
 
+        [EnumDataType(typeof(PaymentMethodTypes), ErrorMessage = "El tipo de coche no es válido.")]
+		public PaymentMethodTypes PaymentMethod { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha de compra")]
+        public DateTime PurchasingDate { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(1, float.MaxValue, ErrorMessage = "El mínimo precio es 1.")]
+        [Display(Name = "Price For Purchase")]
+        [Precision(10, 2)]
+        public decimal PurchasingPrice { get; set; }
+
+        public IList<PurchaseItem> PurchaseItems { get; set; } = new List<PurchaseItem>();
+
+
+
+
+
+
+
+    }
 }
