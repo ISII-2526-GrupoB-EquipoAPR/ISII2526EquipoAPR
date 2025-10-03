56081fca505233d7f1bddf2d455446eaaa66b9b7
ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>
2025-10-03 08:53:24 +0200

    He creado los atributos de la clase Purchase

---
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Purchase
+{
+	public Purchase(){}
+
+	public Purchase (string deliveryCarDealer, int id, string name, string paymentMethod, DateTime purchasingDate, decimal purchasingPrice, string surname)
+	{
+		DeliveryCarDealer = deliveryCarDealer;
+		Id = id;
+		Name = name;
+		PaymentMethod = paymentMethod;
+		PurchasingDate = purchasingDate;
+		PurchasingPrice = purchasingPrice;
+		Surname = surname;
+    }
+
+}
