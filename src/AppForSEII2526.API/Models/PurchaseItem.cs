708fddd4da6021e60eb2988b741757b2d7a446ee
ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>
2025-10-03 08:57:41 +0200

    He creado los atributos de la clase PurchaseItem

---
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class PurchaseItem
+{
+	public PurchaseItem()
+	{
+		public PurchaseItem() { }
+
+		public PurchaseItem(string carId, string purchaseId, int quantity)
+	{
+		CarId = carId;
+		PurchaseId = purchaseId;
+		Quantity = quantity;
+    }
+
+    }
+}
