e084f016ad6e950e5cbddc4d55e1c4f1f3771a5e
ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>
2025-10-06 19:57:41 +0200

    He creado la clase Rental

---
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -0,0 +1,39 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public enum PaymentMethodTypes2
+    {
+        Visa,
+        GooglePay,
+        PayPal
+    }
+    public class Rental
+    {
+        public string deliveryCarDealer { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha de finalización")]
+        public DateTime endDate { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha de comienzo")]
+        public DateTime startDate { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha de renta")]
+        public DateTime rentingDate { get; set; }
+
+        [EnumDataType(typeof(PaymentMethodTypes), ErrorMessage = "El tipo de pago no es válido.")]
+        public PaymentMethodTypes2 paymentMethod { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(1, float.MaxValue, ErrorMessage = "El mínimo precio es 1.")]
+        [Display(Name = "Precio total de renta")]
+        [Precision(10, 2)]
+        public decimal totalPrice { get; set; }
+
+
+    }
+}
