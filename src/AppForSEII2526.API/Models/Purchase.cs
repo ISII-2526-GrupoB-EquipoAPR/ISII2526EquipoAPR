def59d2cee7328778f382722ecaae71007fc2d2e
PATRICIA QUINTANAR MARTÍNEZ <Patricia.Quintanar@alu.uclm.es>
2025-10-07 19:25:02 +0200

    He añadido Id a la clase Purchase

--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -7,7 +7,9 @@
 	}
     public class Purchase
 	{
-		public string DeliveryCarDealer { get; set; }
+        public int Id { get; set; }
+        
+        public string DeliveryCarDealer { get; set; }
 
         [EnumDataType(typeof(PaymentMethodTypes), ErrorMessage = "El tipo de coche no es válido.")]
 		public PaymentMethodTypes PaymentMethod { get; set; }
