2823ac58bf0bb3c80cf26daf8472d100c21eee89
Adela12345678 <166329356+Adela12345678@users.noreply.github.com>
2025-10-07 20:13:58 +0200

    He modificado un atributo de la clase car para el c2 (#58)
    * He modificado la clase Car para el C2
    
    * He modificado un atributo de la clase Car para C2
    
    ---------
    
    Co-authored-by: ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>

--- src/AppForSEII2526.API/Models/Car.cs
+++ src/AppForSEII2526.API/Models/Car.cs
@@ -13,6 +13,8 @@ public class Car
 
     public int Id { get; set; }
 
+    
+
     [StringLength(20, MinimumLength = 1, ErrorMessage = "El fabricante debe tener entre 1 y 20 caracteres.")]
     public string Manufacturer { get; set; }
 
@@ -45,9 +47,7 @@ public class Car
     public Model Model { get; set; }
 
     public IList<PurchaseItem> PurchaseItems { get; set; } = new List<PurchaseItem>();
-    
-    public IList<ReviewItem> ReviewItems { get; set; } = new List<ReviewItem>();
 
-    public IList<RentalItem> RentalItem { get; set; } = new List<RentalItem>();
+    public IList<RentalItem> RentalItems { get; set; } = new List<RentalItem>();
     }
 }
