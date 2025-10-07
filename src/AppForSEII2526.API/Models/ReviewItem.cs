55157e355249f8dc730d9ce7142c63ca1756f262
PATRICIA QUINTANAR MARTÍNEZ <Patricia.Quintanar@alu.uclm.es>
2025-10-07 19:47:05 +0200

    He creado la clase ReviewItem (rubén)

--- src/AppForSEII2526.API/Models/ReviewItem.cs
+++ src/AppForSEII2526.API/Models/ReviewItem.cs
@@ -1,15 +1,16 @@
 ﻿namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(CarId), nameof(ReviewId))]
     public class ReviewItem
     {
         public Car Car { get; set; }
 
         [StringLength(200, ErrorMessage = "La descripción no puede ser mayor de 200 caracteres.")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         public int CarId { get; set; }
 
-        public int ReviewlId { get; set; }
+        public int ReviewId { get; set; }
 
         public Review Review { get; set; }
 
