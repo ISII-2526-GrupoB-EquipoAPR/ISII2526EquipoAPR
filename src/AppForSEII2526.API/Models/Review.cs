3446865ad43f902d293a10f65ab42716e510035f
PATRICIA QUINTANAR MARTÍNEZ <Patricia.Quintanar@alu.uclm.es>
2025-10-07 20:04:06 +0200

    He terminado mi clase Review (rubén)

--- src/AppForSEII2526.API/Models/Review.cs
+++ src/AppForSEII2526.API/Models/Review.cs
@@ -1,5 +1,12 @@
-﻿namespace AppForSEII2526.API.Models
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AppForSEII2526.API.Models
 {
+    public enum DriverTypes
+    {
+          Novato,
+          Experto
+    }
     public class Review
     {
         [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
@@ -7,6 +14,14 @@
         [Display(Name = "Fecha de creación")]
         public DateTime Created { get; set; }
 
+        public int Id { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El país solo puede contener letras.")]
+        [StringLength(25,ErrorMessage = "El nombre del país no puede superar los 25 caracteres.")]
+        public string Country { get; set; }
+
+        [EnumDataType(typeof(DriverTypes), ErrorMessage = "El tipo de conductor no es válido.")]
+        public DriverTypes DriverType { get; set; }
         public IList<ReviewItem> ReviewItems { get; set; } =   new List<ReviewItem>();
     }
 }
