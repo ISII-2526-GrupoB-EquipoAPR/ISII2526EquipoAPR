a2e65b986a0d12b0d37a7632eee2325462d48201
Adela12345678 <166329356+Adela12345678@users.noreply.github.com>
2025-10-07 23:06:04 +0200

    He modificado el atributo Id
    * He creado la clase Rental
    
    * He añadido un cambio a la clase Rental
    
    * He añadido otra modificacion a la clase Rental
    
    * He modificado los atributos de la clase Rental
    
    * He modificado el atributo Id
    
    ---------
    
    Co-authored-by: ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>

--- src/AppForSEII2526.API/Models/Rental.cs
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -9,7 +9,7 @@
 
     public class Rental
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string DeliveryCarDealer { get; set; }
 
         [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
