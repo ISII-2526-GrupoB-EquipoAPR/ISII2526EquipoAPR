ef32f9075dfbf7b49abda015018b5f3628d00904
Ruubencg <146321328+Ruubencg@users.noreply.github.com>
2025-10-07 21:10:11 +0200

    he modificado la clase AplicationDbContex para c4

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,9 +5,12 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<Review> Reviews { get; set; }
+  
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     public DbSet<Car> Cars { get; set; }
     public DbSet<Model> Models { get; set; }
     public DbSet<Purchase> Purchases { get; set; }
     public DbSet<PurchaseItem> PurchaseItems { get; set; }
+    public DbSet<ReviewItem> ReviewItems { get; set; }
 }
