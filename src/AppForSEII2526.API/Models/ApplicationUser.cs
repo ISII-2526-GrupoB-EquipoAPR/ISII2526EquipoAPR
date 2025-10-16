c7ab5815d7cc0c59ab1b9c4490b8b9ff4eeba889
PatriciaQ23 <Patricia.Quintanar@alu.uclm.es>
2025-10-16 22:28:26 +0200

    Revert "De development a main"
    This reverts commit 358b5b49722f923e12eec2d4972bfa618d916e8d.

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -1,26 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
-
-    [Display(Name = "Name")]
-    public string Name
-    {
-        get;
-        set;
-    }
-
-    [Display(Name = "Surname")]
-    public string Surname
-    {
-        get;
-        set;
-    }
-    public IList<Purchase> Purchases { get;set;} = new List<Purchase>();
-    public IList<Review> Reviews { get; set; } = new List<Review>();
-    public IList<Rental> Rentals { get; set; } = new List<Rental>();
-
 }
