26d1e5b50098c96567c161d73f33f595bd042295
PATRICIA QUINTANAR MARTÍNEZ <Patricia.Quintanar@alu.uclm.es>
2025-10-17 11:41:51 +0200

    He borrado y creado la migración otra vez

--- src/AppForSEII2526.API/Migrations/20251017094109_CreateIdentitySchema.Designer.cs
+++ src/AppForSEII2526.API/Migrations/20251017094109_CreateIdentitySchema.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AppForSEII2526.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251016202214_CreateIdentitySchema")]
+    [Migration("20251017094109_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
         /// <inheritdoc />
