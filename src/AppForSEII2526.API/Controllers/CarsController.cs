3e25aa7f2c41e1e32f5c73a28fe75900b76a8ea4
PATRICIA QUINTANAR MARTÍNEZ <Patricia.Quintanar@alu.uclm.es>
2025-10-28 17:03:54 +0100

    He creado el método get de la seleccion para Purchase

---
+++ src/AppForSEII2526.API/Controllers/CarsController.cs
@@ -0,0 +1,112 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        //used to enable your controller to access to the database
+        private readonly ApplicationDbContext _context;
+        //used to log any information when your system is running
+        private readonly ILogger<CarsController> _logger;
+
+        public CarsController(ApplicationDbContext context, ILogger<CarsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        /*[HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2)
+        {
+            if (op2 == 0)
+            {
+                _logger.LogError($"{DateTime.Now} Exception: op2=0, division by 0");
+                return BadRequest("op2 must be different from 0");
+            }
+            decimal result = decimal.Round(op1 / op2, 2);
+            return Ok(result);
+        }
+        */
+        /*
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<Car>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetCarsForPurchasing()
+        {
+            IList<Car> cars = await _context.Cars.ToListAsync();
+            return Ok(cars);
+        }
+        */
+        /*
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<CarForPurchaseDTO>), (int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult> GetCarsForPurchasingDTO()
+        {
+            var coches = await _context.Cars
+                .Select(c => new CarForPurchaseDTO
+                {
+                    Id = c.Id,
+                    Modelo = c.Model.Name,
+                    Color = c.Color,
+                    FuelType = c.FuelType,
+                    Manufacturer = c.Manufacturer,
+                    PurchasePrice = c.PurchasingPrice
+                })
+                .ToListAsync();
+            return Ok(coches);
+
+        }
+        */
+        /*
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<CarForPurchaseDTO>), (int)HttpStatusCode.OK)]
+        
+        public async Task<ActionResult> GetCarsForPurchasingDTO_Filter_Color(string? filtroColor)
+        {
+            var coches = await _context.Cars
+                .Where(c => c.Color.Contains(filtroColor) || (filtroColor == null ) )
+                .Select(c => new CarForPurchaseDTO
+
+        */
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<CarForPurchaseDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetCarForPurchase(string? carColor, string? carModel)
+        {
+            IList<CarForPurchaseDTO> selectCars = await _context.Cars
+
+                .Include(c => c.Model)
+                .Include (c => c.PurchaseItems).ThenInclude(pi => pi.Purchase)
+
+                .Where(c => c.QuantityForPurchasing > 0 &&
+                   (carColor == null || c.Color.Contains(carColor))
+                    && (carModel == null || c.Model.Name.Equals(carModel))
+                    )
+
+                .OrderBy(c => c.Model)
+
+                .Select(c => new CarForPurchaseDTO
+                {
+                    Model = c.Model.Name,
+                    Color = c.Color,
+                    FuelType = c.FuelType,
+                    Manufacturer = c.Manufacturer,
+                    PurchasingPrice = c.PurchasingPrice
+                })
+                
+                .ToListAsync();
+
+            return Ok(selectCars);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/CarForPurchaseDTO.cs
@@ -0,0 +1,48 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class CarForPurchaseDTO
+    {
+        public CarForPurchaseDTO(){
+        }
+
+        public CarForPurchaseDTO(int id, string model, string color, string fuelType, string manufacturer, decimal purchasingPrice)
+        {
+            Id = id;
+            Model = model;
+            Color = color;
+            FuelType = fuelType;
+            Manufacturer = manufacturer;
+            PurchasingPrice = purchasingPrice;
+        }
+        public int Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "El modelo tiene una longitud máxima de 50 caracteres", MinimumLength = 4)]
+        public string Model { get; set; }
+
+        [StringLength(10, MinimumLength = 1, ErrorMessage = "El color debe tener entre 1 y 10 caracteres.")]
+        public string Color { get; set; }
+
+        [StringLength(10, MinimumLength = 1, ErrorMessage = "El tipo de combustible debe tener entre 1 y 10 caracteres.")]
+        public string FuelType { get; set; }
+
+        [StringLength(20, MinimumLength = 1, ErrorMessage = "El fabricante debe tener entre 1 y 20 caracteres.")]
+        public string Manufacturer { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(1, float.MaxValue, ErrorMessage = "El mínimo precio es 1.")]
+        [Display(Name = "Precio de compra")]
+        [Precision(10, 2)]
+        public decimal PurchasingPrice { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CarForPurchaseDTO dTO &&
+                   Id == dTO.Id &&
+                   Model == dTO.Model &&
+                   Color == dTO.Color &&
+                   FuelType == dTO.FuelType &&
+                   Manufacturer == dTO.Manufacturer &&
+                   PurchasingPrice == dTO.PurchasingPrice;
+        }
+    }
+}
