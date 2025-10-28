c9d878986fd67b8d52a3a946b030a0da843f83ca
Ruubencg <146321328+Ruubencg@users.noreply.github.com>
2025-10-28 17:01:45 +0100

    He creado el dto para el get de la selección de review
    * He creado el dto para el get de la selección de review
    
    * Update validation messages in CarForReviewDTO
    
    * Add CarClass property and update validations in DTO

---
+++ src/AppForSEII2526.API/DTOs/CarForReviewDTO.cs
@@ -0,0 +1,58 @@
+﻿using Humanizer.Localisation;
+
+namespace AppForSEII2526.API.DTOs
+{
+    public class CarForReviewDTO
+    {
+        public CarForReviewDTO()
+        {
+
+        }
+
+        public CarForReviewDTO(int id, string Modelo,string CarClass; string Manufacter, string Fueltype, string Color)
+        {
+            Id = id;
+            CarClass = CarClass;
+            Modelo = Modelo;
+            Manufacter = Manufacter;
+            FuelType = FuelType;
+            Color = Color;
+        }
+
+
+public int Id { get; set; }
+
+[StringLength(50, ErrorMessage = "El modelo debe tener una longitud máxima de 50 caracteres")]
+public string Modelo { get; set; }
+[StringLength(50, ErrorMessage = "La clase debe tener una longitud máxima de 50 caracteres")]
+public string CarClass { get; set; }
+[StringLength(50, ErrorMessage = "El fabricante debe tener una longitud máxima de 50 caracteres y mínima de 4", MinimumLength = 4)]
+public string Manufacter { get; set; }
+
+[StringLength(50, ErrorMessage = "El tipo de combustible debe tener una longitud máxima de 50 caracteres")]
+public string FuelType { get; set; }
+
+[StringLength(50, ErrorMessage = "El color debe tener una longitud máxima de 50 caracteres")]
+public string Color { get; set; }
+
+        
+
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CarForReviewDTO dTO &&
+                CarClass == dTO.CarClass
+I                   Id == dTO.Id &&
+                   Modelo == dTO.Modelo &&
+                   Manufacter == dTO.Manufacter &&
+                 FuelType == dTO.FuelType &&
+                   Color == dTO.Color;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Modelo, CarClass, Manufacter, FuelType, Color);
+        }
+    }
+}
