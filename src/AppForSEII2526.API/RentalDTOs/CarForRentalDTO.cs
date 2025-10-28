49b43f3fe767f89fe73f7ba2da9566e6384f118c
Adela12345678 <166329356+Adela12345678@users.noreply.github.com>
2025-10-28 17:12:39 +0100

    111--crear método get de las selección para el caso de uso alquilar coches
    * He modificado CarForRental
    
    * he modificado controller
    
    * He eliminado Carcontroller y he modificado las anotaciones de CarForRental
    
    * He cambiado las anotaciones a español
    
    ---------
    
    Co-authored-by: ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>

---
+++ src/AppForSEII2526.API/RentalDTOs/CarForRentalDTO.cs
@@ -0,0 +1,73 @@
+﻿using System.Drawing;
+
+namespace AppForSEII2526.API.DTOs
+{
+    public class CarForRentalDTO
+    {
+        public CarForRentalDTO()
+        {
+        }
+
+        public CarForRentalDTO(int id, string model, string fuelType, string color, DateTime rentingDate, decimal priceForRenting)
+        {
+            Id = id;
+            Model = model;
+            FuelType = fuelType;
+            Color = color;
+            RentingDate = rentingDate;
+            PriceForRenting = priceForRenting;
+        }
+        public CarForRentalDTO(int id, string model, string color, DateTime rentingDate, double priceForRenting, DateTime? lastRental) : this(id, model, color, rentingDate, priceForRenting)
+        {
+            LastRental = lastRental;
+        }
+
+        public int Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "Modelo puede tener como maximo 50 caracteres")]
+        public string Model { get; set; }
+
+        [StringLength(50, ErrorMessage = "Tipo de gasoil puede tener como maximo 50 caracteres")]
+        public string FuelType { get; set; }
+
+        [StringLength(50, ErrorMessage = "Color puede tener como maximo 50 caracteres")]
+        public string Color { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha para alquilar")]
+        public DateTime RentingDate { get; set; }
+
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(1, float.MaxValue, ErrorMessage = "Precio minimo es 1")]
+        [Display(Name = "Precio para alquilar")]
+        public decimal PriceForRenting { get; set; }
+
+
+        public DateTime? LastRental { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CarForRentalDTO dTO
+                && Id == dTO.Id
+                && Model == dTO.Model
+                && FuelType == dTO.FuelType
+                && Color == dTO.Color
+                && RentingDate == dTO.RentingDate
+                && PriceForRenting == dTO.PriceForRenting;
+          
+
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Model, FuelType, Color, RentingDate, PriceForRenting);
+        }
+
+
+
+
+
+
+    }
+}
