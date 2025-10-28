e05c14ae7a61c799b9890ac3d1d7cf95604d3477
PatriciaQ23 <Patricia.Quintanar@alu.uclm.es>
2025-10-28 18:46:39 +0100

    He creado PurchaseItemDTO

---
+++ src/AppForSEII2526.API/DTOs/PurchaseDTOs/PurchaseItemDTO.cs
@@ -0,0 +1,29 @@
+﻿namespace AppForSEII2526.API.DTOs.PurchaseDTOs
+{
+    public class PurchaseItemDTO
+    {
+        public PurchaseItemDTO(int carID, string model, string color, decimal purchasingPrice, int quantity, string description)
+        {
+            CarID = carID;
+            Model = model;
+            Color = color;
+            PurchasingPrice = purchasingPrice;
+            Description = description;
+            Quantity = quantity;
+        }
+
+        public int CarID { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public decimal PurchasingPrice { get; set; }
+
+        public string Description { get; set; }
+
+        public int Quantity { get; set; }
+
+
+    }
+}
