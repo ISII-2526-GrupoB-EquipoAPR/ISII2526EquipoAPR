aa39b7c3856b5014f43aa40298b426f704a02f8f
Ruubencg <146321328+Ruubencg@users.noreply.github.com>
2025-10-28 16:48:19 +0100

    Update validation messages in CarForReviewDTO

--- src/AppForSEII2526.API/DTOs/CarForReviewDTO.cs
+++ src/AppForSEII2526.API/DTOs/CarForReviewDTO.cs
@@ -21,16 +21,16 @@ namespace AppForSEII2526.API.DTOs
 
         public int Id { get; set; }
 
-        [StringLength(50, ErrorMessage = "Title must have a maximun length of 50 characters")]
+        [StringLength(50, ErrorMessage = "Modelo must have a maximun length of 50 characters")]
         public string Modelo { get; set; }
 
-        [StringLength(50, ErrorMessage = "Genre must have a maximun length of 50 characters", MinimumLength = 4)]
+        [StringLength(50, ErrorMessage = "Manufacter must have a maximun length of 50 characters", MinimumLength = 4)]
         public string Manufacter { get; set; }
 
-        [StringLength(50, ErrorMessage = "Title must have a maximun length of 50 characters")]
+        [StringLength(50, ErrorMessage = "Fueltype must have a maximun length of 50 characters")]
         public string FuelType { get; set; }
 
-        [StringLength(50, ErrorMessage = "Title must have a maximun length of 50 characters")]
+        [StringLength(50, ErrorMessage = "Color must have a maximun length of 50 characters")]
         public string Color { get; set; }
 
 
