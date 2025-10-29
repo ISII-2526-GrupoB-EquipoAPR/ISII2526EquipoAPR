5ea1c988b4d59fc5a20194c6948c4c8e928a80fb
ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>
2025-10-29 13:49:02 +0100

    He creado RentalItemDTO y lo he implementado

---
+++ src/AppForSEII2526.API/DTOs/RentalDTOs/RentalItemDTO.cs
@@ -0,0 +1,47 @@
+﻿using AppForSEII2526.API.Models;
+
+namespace AppForSEII2526.API.DTOs.RentalDTOs
+{
+    public class RentalItemDTO
+    {
+        public RentalItemDTO(int carId,string model, string manufacturer, decimal rentingPrice,int quantity)
+        {
+            CarId = carId;
+            Model = model;
+            Manufacturer = manufacturer;
+            RentingPrice = rentingPrice;
+            Quantity = quantity;
+
+        }
+        public int CarId { get; set; }
+        public string Model { get; set; }
+
+
+        public string Manufacturer { get; set; }
+
+
+        public decimal RentingPrice { get; set; }
+
+        public int Quantity { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is RentalItemDTO dTO
+                   && CarId == dTO.CarId 
+                   && Model == dTO.Model
+                   && Manufacturer == dTO.Manufacturer
+                   && RentingPrice == dTO.RentingPrice
+                   && Quantity == dTO.Quantity;
+
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(CarId,Model, Manufacturer, RentingPrice,Quantity);
+        }
+    }
+}
+
+
+    }
+}
