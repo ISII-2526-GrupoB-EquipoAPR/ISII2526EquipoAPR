7cd785131b2555f587e1b30e1b24306f92f83d82
ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>
2025-10-30 18:57:08 +0100

    He implementado la clase RentalForCreateDTO

---
+++ src/AppForSEII2526.API/DTOs/RentalDTOs/RentalForCreateDTO.cs
@@ -0,0 +1,46 @@
+﻿namespace AppForSEII2526.API.DTOs.RentalDTOs
+{
+    public class RentalForCreateDTO
+    {
+        public RentalForCreateDTO(string customerUserName, string customerNameSurname, string deliveryAddress, PaymentMethodTypes paymentMethod)
+        {
+            CustomerUserName = customerUserName ?? throw new ArgumentNullException(nameof(customerUserName)); ;
+            CustomerNameSurname = customerNameSurname ?? throw new ArgumentNullException(nameof(customerNameSurname)); ;
+            DeliveryAddress = deliveryAddress ?? throw new ArgumentNullException(nameof(deliveryAddress)); ;
+            PaymentMethod = paymentMethod;
+        }
+
+
+
+        [EmailAddress]
+        [Required]
+        public string CustomerUserName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Porfavor, escriba su nombre y apellido")]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "Nombre y apellidos")]
+        public string CustomerNameSurname { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.MultilineText)]
+        [Display(Name = "Delivery Address")]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "La direccion debe tener 10 caracteres")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Porfavor, escirba su direccion")]
+        public string DeliveryAddress { get; set; }
+
+
+        public PaymentMethodTypes PaymentMethod { get; set; }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is RentalForCreateDTO dTO 
+                   && CustomerUserName == dTO.CustomerUserName
+                   && CustomerNameSurname == dTO.CustomerNameSurname
+                   && DeliveryAddress == dTO.DeliveryAddress
+                   && PaymentMethod == dTO.PaymentMethod;
+
+
+        }
+    }
+
+}
+
