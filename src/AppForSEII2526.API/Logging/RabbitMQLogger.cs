96a6bcbb582a042ed4caf1d39f27c6374d5075ca
ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>
2025-11-10 19:31:36 +0100

    He hecho la parte 1 de la p3

--- src/AppForSEII2526.API/Logging/RabbitMQLogger.cs
+++ src/AppForSEII2526.API/Logging/RabbitMQLogger.cs
@@ -3,6 +3,7 @@ using System.Text.Json;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
+using IModel = RabbitMQ.Client.IModel;
 
 namespace AppForSEII2526.API.Logging;
 
@@ -18,8 +19,31 @@ public class RabbitMQLogger : ILogger, IDisposable
     {
         _name = name ?? throw new ArgumentNullException(nameof(name));
         _config = config ?? throw new ArgumentNullException(nameof(config));
-        
-        ValidateConfiguration(_config); 
+
+        ValidateConfiguration(_config);
+
+        var factory = new ConnectionFactory
+        {
+            HostName = _config.HostName,
+            Port = _config.Port,
+            UserName = _config.UserName,
+            Password = _config.Password
+        };
+
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+
+        _channel.ExchangeDeclare(
+            exchange: _config.Exchange,
+            type: _config.ExchangeType,
+            durable: _config.Durable);
+
+        _properties = _channel.CreateBasicProperties();
+        _properties.Persistent = true;
+        _properties.ContentType = "application/json";
+
+      
+
     }
 
     private static void ValidateConfiguration(RabbitMQLoggerConfiguration config)
@@ -36,7 +60,9 @@ public class RabbitMQLogger : ILogger, IDisposable
             throw new ArgumentException("RabbitMQ Exchange is required", nameof(config));
         if (string.IsNullOrEmpty(config.ExchangeType))
             throw new ArgumentException("RabbitMQ ExchangeType is required", nameof(config));
+
     }
+
 
     public IDisposable? BeginScope<TState>(TState state) where TState : notnull => default;
 
@@ -65,11 +91,22 @@ public class RabbitMQLogger : ILogger, IDisposable
                 Exception = exception?.ToString()
             };
 
+            var message = JsonSerializer.Serialize(logEntry);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            _channel.BasicPublish(
+                 exchange: _config.Exchange,
+                 routingKey: "",
+                 basicProperties: _properties,
+                 body: body);
+
         }
         catch (Exception ex)
         {
             Console.Error.WriteLine($"Error publishing log message to RabbitMQ: {ex.Message}");
         }
+
+
     }
 
     public void Dispose()
