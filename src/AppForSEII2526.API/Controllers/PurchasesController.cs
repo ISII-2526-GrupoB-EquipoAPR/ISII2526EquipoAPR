5e47d38250b2bdbaff49f8079bae3f76b1531da5
Adela12345678 <166329356+Adela12345678@users.noreply.github.com>
2025-11-11 08:51:00 +0100

    Proyecto api funcionamiento del provider adela
    * hemos modificado cosas
    
    * He hecho la parte 1 de la p3
    
    ---------
    
    Co-authored-by: ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>

--- src/AppForSEII2526.API/Controllers/PurchasesController.cs
+++ src/AppForSEII2526.API/Controllers/PurchasesController.cs
@@ -24,31 +24,40 @@ namespace AppForSEII2526.API.Controllers
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<ActionResult> GetPurchase(int id)
         {
-            if (_context.Purchases == null)
+            try
             {
-                _logger.LogError("Error: Purchases table does not exist");
+                if (_context.Purchases == null)
+                {
+                    _logger.LogError("Error: Purchases table does not exist");
+                    return NotFound();
+                }
+
+                var purchase = await _context.Purchases
+                 .Where(p => p.Id == id)
+                     .Include(p => p.PurchaseItems) //join table PurchaseItems
+                        .ThenInclude(pi => pi.Car) //then join table Car
+                            .ThenInclude(car => car.Model) //then join table Model
+                 .Select(p => new PurchaseDetailDTO(p.Id, p.CustomerUserName,
+                        p.CustomerNameSurname, (PaymentMethodTypes)p.PaymentMethod, p.DeliveryAddress, p.PurchasingDate, p.PurchaseItems
+                            .Select(pi => new PurchaseItemDTO(pi.Car.Id, pi.Car.Model.Name, pi.Car.Color, pi.Car.PurchasingPrice, pi.Quantity, pi.Car.Description)).ToList<PurchaseItemDTO>()))
+                 .FirstOrDefaultAsync();
+
+
+                if (purchase == null)
+                {
+                    _logger.LogError($"Error: Purchase with id {id} does not exist");
+                    return NotFound();
+                }
+
+                _logger.LogInformation($"Purchase with id {id} obtained successfully"); 
+
+                return Ok(purchase);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
                 return NotFound();
             }
-
-            var purchase = await _context.Purchases
-             .Where(p => p.Id == id)
-                 .Include(p => p.PurchaseItems) //join table PurchaseItems
-                    .ThenInclude(pi => pi.Car) //then join table Car
-                        .ThenInclude(car => car.Model) //then join table Model
-             .Select(p => new PurchaseDetailDTO(p.Id, p.CustomerUserName,
-                    p.CustomerNameSurname, (PaymentMethodTypes)p.PaymentMethod, p.DeliveryAddress, p.PurchasingDate, p.PurchaseItems
-                        .Select(pi => new PurchaseItemDTO(pi.Car.Id, pi.Car.Model.Name, pi.Car.Color, pi.Car.PurchasingPrice, pi.Quantity, pi.Car.Description)).ToList<PurchaseItemDTO>()))
-             .FirstOrDefaultAsync();
-
-
-            if (purchase == null)
-            {
-                _logger.LogError($"Error: Purchase with id {id} does not exist");
-                return NotFound();
-            }
-
-
-            return Ok(purchase);
         }
 
         [HttpPost]
