b4de0485c9981214642a71e3a5655b00ea0abecd
Adela12345678 <166329356+Adela12345678@users.noreply.github.com>
2025-11-14 18:44:39 +0100

    He implementado GetCarForRental_test
    * He implementado GetCarForRental_test
    
    * He creado un constructor en car para que solo tenga priceforrenting y quantity for renting (ademas de los otros atributos)
    
    * Pasa igual q en el otr test, si quito el constructore de patricia me deja mas menos pero sino no. Además los coches me salen cambiados de orden.
    
    * Sigue sin funcionar
    
    * Me salen al reves los expected y los carDTOActual
    
    * me va 1 bien el 0
    
    * Me sigues dando uno bien
    
    * Ya me va todo correctoooo.Para que me funcione individualmente hay q comentar el constructor de la otra persona, sino a mí me coge el de patricia y no el mío
    
    * Ya va todo bien bien
    
    ---------
    
    Co-authored-by: ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>

--- src/AppForSEII2526.API/Models/Car.cs
+++ src/AppForSEII2526.API/Models/Car.cs
@@ -33,24 +33,19 @@ public class Car
             RimSize = rimSize;
             Model = model;
         }
-
-        public Car(string carClass, string color, string? description, string engDisplacement, string manufacturer, decimal rentingPrice, int quantityForRenting, string fuelType, string rimSize, Model model)
+        public Car(decimal rentingPrice, string carClass, string color, string? description, string engDisplacement, string manufacturer, int quantityForRenting, string fuelType, string rimSize, Model model)
         {
             CarClass = carClass;
             Color = color;
             Description = description;
             EngDisplacement = engDisplacement;
-            Manufacturer = manufacturer;
-            RentingPrice = rentingPrice;
+            Manufacturer = manufacturer;          
+            RentingPrice = rentingPrice;          
             QuantityForRenting = quantityForRenting;
             FuelType = fuelType;
             RimSize = rimSize;
             Model = model;
-
-           
         }
-
-
 
 
         [StringLength(10, MinimumLength = 1, ErrorMessage = "La clase del coche debe tener entre 1 y 10 caracteres.")]
