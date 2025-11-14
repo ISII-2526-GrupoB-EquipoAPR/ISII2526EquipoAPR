74c2faf80aede4e151f31c0135c07ebd0a4855f5
ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>
2025-11-14 12:20:28 +0100

    Ya me va todo correctoooo.Para que me funcione individualmente hay q comentar el constructor de la otra persona, sino a mí me coge el de patricia y no el mío

--- test/AppForSEII2526.UT/CarController_test/GetCarsForRental_test.cs
+++ test/AppForSEII2526.UT/CarController_test/GetCarsForRental_test.cs
@@ -41,10 +41,11 @@ namespace AppForSEII2526.UT.CarController_test
             var carDTOs = new List<CarForRentalDTO>()
             {
 
-                    new CarForRentalDTO(4, "CX-5",    "Gasolina", "Gris",   60000, "Mazda"),     // ← Posición 0
-                    new CarForRentalDTO(1, "Yaris",   "Híbrido",  "Azul",   45000, "Toyota"),    // ← Posición 1  
-                    new CarForRentalDTO(3, "Civic",   "Gasolina", "Rojo",   50000, "Honda"),     // ← Posición 2
-                    new CarForRentalDTO(2, "Model 3", "Eléctrico","Blanco", 80000, "Tesla")      // ← Posición 3
+                     new CarForRentalDTO(1, "Yaris",   "Híbrido",  "Azul",   45000, "Toyota"),
+                     new CarForRentalDTO(2, "Model 3", "Eléctrico","Blanco", 80000, "Tesla"),
+                     new CarForRentalDTO(4, "CX-5",    "Gasolina", "Gris",   60000, "Mazda"),
+                     new CarForRentalDTO(3, "Civic",   "Gasolina", "Rojo",   50000, "Honda")    
+
 
                 
             };
@@ -53,13 +54,13 @@ namespace AppForSEII2526.UT.CarController_test
             var carDTOsTC1 = new List<CarForRentalDTO>() { carDTOs[0], carDTOs[1], carDTOs[2], carDTOs[3] };
 
             var carDTOsTC2 = new List<CarForRentalDTO>() { carDTOs[0] };
-            var carDTOsTC3 = new List<CarForRentalDTO>() { carDTOs[2]};
+            var carDTOsTC3 = new List<CarForRentalDTO>() { carDTOs[0],carDTOs[2],carDTOs[3]};
 
             var allTests = new List<object[]>
             {             //filters to apply - expected cars
                 new object[] { null, null, carDTOsTC1 },
                 new object[] { "Yaris", null, carDTOsTC2},
-                new object[] { null, 50000, carDTOsTC3},
+                new object[] { null, 60000m, carDTOsTC3},
             };
 
             return allTests;
