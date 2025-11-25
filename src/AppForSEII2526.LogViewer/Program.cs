18a25c729f32e5e13107eeb3f68de89650ae6bf4
PatriciaQ23 <Patricia.Quintanar@alu.uclm.es>
2025-11-25 11:47:24 +0100

    Práctica 3
    * hemos modificado cosas
    
    * He hecho la parte 1 de la p3
    
    * Terminado
    
    * Terminada P3
    
    ---------
    
    Co-authored-by: ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>

---
+++ src/AppForSEII2526.LogViewer/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AppForSEII2526.LogViewer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Subscriber subscriber = new Subscriber();
+        try
+        {
+            subscriber.StartConsuming();
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadLine();
+        }
+        finally { }
+    }
+}
