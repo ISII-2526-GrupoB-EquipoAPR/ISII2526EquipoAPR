ab384f2cb74640b3b9a2b83daf138ed6a490cd96
PatriciaQ23 <Patricia.Quintanar@alu.uclm.es>
2025-12-04 21:05:33 +0100

    He creado ModelsController (#270)

---
+++ src/AppForSEII2526.API/Controllers/ModelsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    public class ModelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private ILogger _logger;
+
+        public ModelsController(ApplicationDbContext context, ILogger<ModelsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/Models/GetModelsForPurchase
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<string>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetModels (string? modelName)
+        {
+
+            IList<string> models = await _context.Models
+                .Where(model => (modelName == null || model.Name.Contains(modelName))) // where clause             
+                .OrderBy(model => model.Name)
+                .Select(model => model.Name)
+                .ToListAsync();
+
+            return Ok(models);
+        }
+    }
+}
