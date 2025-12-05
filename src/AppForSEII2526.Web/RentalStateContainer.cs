edd286eb6b830738531e541165d8b8127f7a53eb
Adela12345678 <166329356+Adela12345678@users.noreply.github.com>
2025-12-05 08:43:15 +0100

    El RentalStateContainer ha sido implementado (#275)
    Co-authored-by: ADELA JEREZ SÁNCHEZ <Adela.Jerez@alu.uclm.es>

---
+++ src/AppForSEII2526.Web/RentalStateContainer.cs
@@ -0,0 +1,63 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+
+{
+    public class RentalStateContainer
+    {
+        public RentalForCreateDTO Rental { get; private set; } = new RentalForCreateDTO()
+        {
+            RentalItems = new List<RentalItemDTO>()
+        };
+
+        
+        public decimal TotalPrice
+        {
+            get
+            {
+                int numberOfDays = (Rental.EndDate - Rental.StartDate).Days;
+                return Convert.ToDecimal(Rental.RentalItems.Sum(ri => ri.RentingPrice * numberOfDays));
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+
+        public void AddCarToRental(CarForRentalDTO car)
+        {
+            if (!Rental.RentalItems.Any(ri => ri.Model == car.Model))
+                Rental.RentalItems.Add(new RentalItemDTO()
+                {
+                    Model = car.Model,
+                    Manufacturer = car.Manufacturer,
+                    RentingPrice = car.PriceForRenting
+                }
+            );
+
+        }
+
+        public void RemoveRentalItemToRent(RentalItemDTO item)
+        {
+            Rental.RentalItems.Remove(item);
+
+        }
+
+        public void ClearRentingCart()
+        {
+            Rental.RentalItems.Clear();
+
+        }
+
+        public void RentalProcessed()
+        {
+            Rental = new RentalForCreateDTO()
+            {
+                RentalItems = new List<RentalItemDTO>()
+            };
+        }
+    }
+}
+    
