69cc3179e25e8ca2a80ecb02cc0888b16d323f84
ruubencg <Ruben.Cuesta3@alu.uclm.es>
2025-12-12 02:40:22 +0100

    .

---
+++ src/AppForSEII2526.Web/ReviewStateContainer.cs
@@ -0,0 +1,62 @@
+﻿using AppForSEII2526.API.DTOs.ReviewDTOs;
+using AppForSEII2526.API.Models;
+using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class ReviewStateContainer
+    {
+        public ReviewForCreateDTO Review { get; set; } = new ReviewForCreateDTO()
+        {
+            ReviewItems = new List<ReviewItemsDTO>()
+        };
+
+
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AddCarToReview(CarForReviewDTO car)
+        {
+            if (!Review.ReviewItems.Any(ri => ri.Model == car.Modelo))
+                Review.ReviewItems.Add(new ReviewItemsDTO()
+                {
+                    
+                Model=car.Modelo,
+                    
+                    Color = car.Color,
+                    Fueltype = car.FuelType,
+                    Manufacturer = car.Manufacturer,
+                    
+
+                }
+            );
+
+        }
+
+        //to delete cars from the list of selected cars
+        public void RemoveReviewItemToReview(ReviewItemsDTO item)
+        {
+            Review.ReviewItems.Remove(item);
+
+        }
+        //we eliminate all the cars from the list
+        public void ClearReviewCart()
+        {
+            Review.ReviewItems.Clear();
+
+        }
+
+        //we have already finished the process of purchasing, thus, we create a new Purchase 
+        public void ReviewProcessed()
+        {
+
+            Review = new ReviewForCreateDTO()
+            {
+                ReviewItems = new List<ReviewItemsDTO>()
+            };
+        }
+
+    }
+}
