487291844ade42c4b6133d78527d090a6206dd37
Patricia <Patricia.Quintanar@alu.uclm.es>
2025-12-16 11:08:16 +0100

    Creados los PO y la primera prueba funcional

---
+++ test/AppForSEII2526.UIT/Purchase/SelectCarsForPurchase_PO.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.PurchaseCars
+{
+    public class SelectCarsForPurchase_PO : PageObject
+    {
+        private By _carColorBy = By.Id("carColor");
+        private By _carModelBy = By.Id("carModel");
+
+        private By _ShowPurchasingCartBy = By.Id("showPurchasingCart");
+        private By _searchCarsBy = By.Id("searchCars");
+        private By _purchaseButtonBy = By.Id("Purchase");
+        
+        private By _tableOfCarsBy = By.Id("TableOfCars");
+        private By _modalBy = By.Id("DialogOkSaveDelete");
+
+        private IWebElement _carModel() => _driver.FindElement(_carModelBy);
+
+        private IWebElement _carColor() => _driver.FindElement(_carColorBy);
+        private IWebElement _showPurchasingCartButton() => _driver.FindElement(_ShowPurchasingCartBy);
+        private IWebElement _searchCarButton() => _driver.FindElement(_searchCarsBy);
+        private IWebElement _purchaseButton() => _driver.FindElement(_purchaseButtonBy);
+
+
+
+        public SelectCarsForPurchase_PO(IWebDriver driver, ITestOutputHelper output)
+            : base(driver, output)
+        {
+
+        }
+
+        public void FilterCars (string carModel, string carColor)
+        {
+            WaitForBeingVisible(_carColorBy);
+
+            _carColor().SendKeys(carColor);
+
+            if (carModel == "") carModel = "All";
+
+            SelectElement selectElement = new SelectElement(_carModel());
+            selectElement.SelectByText(carModel);
+
+            _searchCarButton().Click();
+
+            System.Threading.Thread.Sleep(2000);
+
+        }
+
+        public void SelectCars(List<string> carModels)
+        {
+            //we wait for till the movies are available to be selected 
+            foreach (var carModel in carModels)
+            {
+                WaitForBeingVisible(By.Id($"carToPurchase_{carModel}"));
+                _driver.FindElement(By.Id($"carToPurchase_{carModel}")).Click();
+            }
+        }
+
+        public void PurchaseCars()
+        {
+            WaitForBeingClickable(_purchaseButtonBy);
+            _purchaseButton().Click();
+        }
+
+        public void ModifyRentingCart(string model)
+        {
+            _showPurchasingCartButton().Click();
+            WaitForBeingVisible(By.Id($"removeCar_{model}"));
+            _driver.FindElement(By.Id($"removeCar_{model}")).Click();
+        }
+
+        public bool CheckListOfCars(List<string[]> expectedCars)
+        {
+
+            return CheckBodyTable(expectedCars, _tableOfCarsBy);
+        }
+
+        public bool CheckPurchaseCarsDisabled()
+        {
+            //we return true if the button is disabled
+            return !(_purchaseButton().Enabled);
+        }
+
+        public bool CheckShoppingCart(string price)
+        {
+            return _showPurchasingCartButton().Text.Contains(price);
+        }
+
+        public bool CheckMessageErrorNotAvaibleMovies(string expectedError)
+        {
+            return _driver.PageSource.Contains(expectedError);
+
+        }
+
+        public bool CheckMessageError(string expectedError)
+        {
+            return CheckModalBodyText(expectedError, _modalBy);
+        }
+
+    }
+}
---
+++ test/AppForSEII2526.UIT/Purchase/UCPurchaseCars_UIT.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.PurchaseCars
+{
+    public class UCPurchaseCars_UIT : UC_UIT
+    {
+        private SelectCarsForPurchase_PO selectcars;
+        
+        public UCPurchaseCars_UIT(ITestOutputHelper output) : base(output)
+        {
+            Initial_step_opening_the_web_page();
+            selectcars = new SelectCarsForPurchase_PO(_driver, _output);
+           
+        }
+
+        private const int carId1 = 14;
+        private const string color1 = "Blanco";
+        private const string carModel1 = "Q3";
+        private const string purchasingPrice1 = "42000";
+        private const string manufacturer1 = "Audi";
+        
+        
+        private const int carId2 = 2;
+        private const string color2 = "Rojo";
+        private const string carModel2 = "Golf";
+        private const string purchasingPrice2 = "20000";
+        private const string manufacturer2 = "Volkswagen";
+
+        private const string quantity = "1";
+        
+        private void Precondition_perform_login()
+        {
+            Perform_login("elena@uclm.es", "Password1234%");
+        }
+
+        private void InitialStepsForPurchaseCars()
+        {
+            Precondition_perform_login();
+            selectcars.WaitForBeingVisibleIgnoringExeptionTypes(By.Id("CreatePurchase"));
+            selectcars.WaitForBeingClickable(By.Id("CreatePurchase"));
+            _driver.FindElement(By.Id("CreatePurchase")).Click();
+        }
+
+        [Theory]
+        [InlineData("Elena Navarro", "Calle de la Universidad 1, Albacete, 02006, España", "Visa")]
+        [InlineData("Elena Navarro", "Calle de la Universidad 1, Albacete, 02006, España", "Paypal")]
+        [InlineData("Elena Navarro", "Calle de la Universidad 1, Albacete, 02006, España", "GooglePay")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC1_BF_1_2_3 (string surname, string deliveryAddress, string paymentMethod)
+        {
+            //Arrange
+            var createpurchase = new CreatePurchase_PO(_driver, _output);
+            var detailPurchase = new DetailPurchase_PO(_driver, _output);
+
+            //Act
+            InitialStepsForPurchaseCars();
+
+            selectcars.SelectCars(new List<string> { carModel1 });
+            selectcars.PurchaseCars();
+
+            createpurchase.FillInPurchaseInfo(surname, deliveryAddress, paymentMethod);
+            createpurchase.FillInPurchaseQuantity(quantity, carModel1);
+            createpurchase.PressPurchaseYourCars();
+            createpurchase.PressOkModalDialog();
+
+            //Assert
+            //the expected error is shown in the view
+            Assert.True(detailPurchase.CheckPurchaseDetail(surname,
+                deliveryAddress, paymentMethod, DateTime.Now, purchasingPrice1 + " €"),
+                "Error: detail purchase is not as expected");
+
+            var expectedRentalItems = new List<string[]>
+                    { new string[] { carModel1, purchasingPrice1 + " €" , color1, quantity}, };
+
+            Assert.True(detailPurchase.CheckListOfPurchase(expectedRentalItems),
+                "Error: purchase items are not as expected");
+        }
+    }
+}
